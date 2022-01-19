3440514537818add5b3836c2570bc7224e76e1a2
edwinkim97 <edwinkim124@gmail.com>
2022-01-18 16:52:59 -0800

    finished giphy party

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 
 $("#submit-search").on("click", handleSearch);
 
+$("#clear-gifs").on("click", clearGifs);
+
+/** Handles click on submit button,
+ * returns gif image from Giphy API,
+ * and adds it to our list of gifs on DOM
+ */
 async function handleSearch(evt) {
     evt.preventDefault();
     const searchTerm = $("#search-input").val();
-    console.log(searchTerm);
     let response = await axios.get("http://api.giphy.com/v1/gifs/search", 
         {params: {"q": searchTerm, "api_key": "GcAhuzzHGsYT5TKbnw0w1VdlhevbqF4D"}});
-    console.log(response);
-    console.log(response.data.data[0].embed_url);
+    let gif = $(`<img src=${response.data.data[0].images.original.url}>`);
+    $("#gif-container").append(gif);
+}
+
+/** Clears gif container on click */
+function clearGifs() {
+    $("#gif-container").empty();
 }
