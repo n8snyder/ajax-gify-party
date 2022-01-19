d5bb7e1d16a01a732aa6dee2d048a99dbd63ba65
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-18 16:18:20 -0800

    Add search and callback for getting giphy image

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Let's get this party started!");
+"use strict";
+
+$("#submit-search").on("click", handleSearch);
+
+async function handleSearch(evt) {
+    evt.preventDefault();
+    const searchTerm = $("#search-input").val();
+    console.log(searchTerm);
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", 
+        {params: {"q": searchTerm, "api_key": "GcAhuzzHGsYT5TKbnw0w1VdlhevbqF4D"}});
+    console.log(response);
+    console.log(response.data.data[0].embed_url);
+}
